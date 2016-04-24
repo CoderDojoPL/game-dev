e731cce1cb62a4fb89e698e7edd9d2bfbc9d3c2a
Michał Tomczak <michal@tomczak.net.pl>
2016-04-24 10:34:53 +0200

    Added documentation

--- game.js
+++ game.js
@@ -1,9 +1,35 @@
+/*
+ * This file is part of the Game-dev project.
+ *
+ * (c) CoderDojo Polska Foundation
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+/**
+ * Character class
+ *
+ * @author Michal Tomczak (m.tomczak@coderdojo.org.pl)
+ */
 function Character(x,y,engine){
+	/**
+	 * @var private attr
+	 */
 	var prv={};
+
+	/**
+	 * @var public attr
+	 */
 	var pub=this;
 
-
+	/**
+	 * Constructor
+	 *
+	 * @param int x
+	 * @param int y
+	 * @param Game engine
+	 */
 	prv.init=function(x,y,engine){
 		prv.direction=1;
 		prv.image=new Image();
@@ -21,7 +47,12 @@ function Character(x,y,engine){
 		prv.isKick=false;
 	}
 
-
+	/**
+	 * Character controller
+	 *
+	 * @param long drawStart current epoch
+	 * @param long diff diffrent time between frames
+	 */
 	pub.controller=function(drawStart,diff){
 
 		var inputManager=prv.engine.getInputManager();
@@ -75,7 +106,6 @@ function Character(x,y,engine){
 
 		}
 
-		//skok
 		if(prv.isJump){
 			var y=prv.pos.y-(40/diff);
 			if(!prv.engine.getGround().isColision({x:prv.pos.x,y:y+pub.getHeight()},1) && !prv.engine.getGround2().isColision({x:prv.pos.x,y:y+pub.getHeight()},1)){
@@ -90,7 +120,7 @@ function Character(x,y,engine){
 			}
 		}
 
-		//grawitacja
+		//gravity
 		if(!prv.isJump){
 			var y=prv.pos.y+(40/diff);
 			if(!prv.engine.getGround().isColision({x:prv.pos.x,y:y+pub.getHeight()},-1) && !prv.engine.getGround2().isColision({x:prv.pos.x,y:y+pub.getHeight()},-1)){
@@ -110,6 +140,13 @@ function Character(x,y,engine){
 
 	}
 
+	/**
+	 * Redraw character
+	 *
+	 * @param CanvasRenderingContext2D context
+	 * @param long drawStart epoch
+	 * @param long diff diffrent time between frames
+	 */
 	pub.update=function(context,drawStart,diff){
 		if(prv.isRestore){
 			prv.updateRestore(context,drawStart,diff);
@@ -135,21 +172,35 @@ function Character(x,y,engine){
 		}
 	}
 
+	/**
+	 * Character run animation
+	 *
+	 * @param CanvasRenderingContext2D context
+	 * @param long drawStart epoch
+	 * @param long diff diffrent time between frames
+	 */
 	prv.updateRun=function(context,drawStart,diff){
 		var frameTime=150;
 		var maxFrame=10;
 		var duration=drawStart-prv.stateTime;
 		var milisecondFrames=duration%(frameTime*maxFrame);
 		var currentFrame=parseInt(milisecondFrames/(frameTime));
-		context.drawImage(prv.image //grafika
-			,currentFrame*44 , pub.getHeight()*(prv.direction>0?3:4) //punkt od którego kadrować
-			,44,pub.getHeight() //skala obrazka
-			,prv.pos.x,prv.pos.y //współrzędne rysowania
-			,44,pub.getHeight()//wielkość obrazka
+		context.drawImage(prv.image //image
+			,currentFrame*44 , pub.getHeight()*(prv.direction>0?3:4) //cut point
+			,44,pub.getHeight() //image scale
+			,prv.pos.x,prv.pos.y //image coordiate
+			,44,pub.getHeight()//image size
 		);
 
 	}
 
+	/**
+	 * Character stay animation
+	 *
+	 * @param CanvasRenderingContext2D context
+	 * @param long drawStart epoch
+	 * @param long diff diffrent time between frames
+	 */
 	prv.updateStay=function(context,drawStart,diff){
 
 		var frameTime=150;
@@ -161,15 +212,22 @@ function Character(x,y,engine){
 			currentFrame=0;
 		}
 
-		context.drawImage(prv.image //grafika
-			,currentFrame*44 , pub.getHeight()*(prv.direction>0?1:2) //punkt od którego kadrować
-			,44,pub.getHeight() //skala obrazka
-			,prv.pos.x,prv.pos.y //współrzędne rysowania
-			,44,pub.getHeight()//wielkość obrazka
+		context.drawImage(prv.image //image
+			,currentFrame*44 , pub.getHeight()*(prv.direction>0?1:2) //cut point
+			,44,pub.getHeight() //image scale
+			,prv.pos.x,prv.pos.y //coordinate draw
+			,44,pub.getHeight()//image size
 		);
 
 	}
 
+	/**
+	 * Character jump animation
+	 *
+	 * @param CanvasRenderingContext2D context
+	 * @param long drawStart epoch
+	 * @param long diff diffrent time between frames
+	 */
 	prv.updateJump=function(context,drawStart,diff){
 
 		var frameTime=150;
@@ -177,60 +235,86 @@ function Character(x,y,engine){
 		var duration=drawStart-prv.stateTime;
 		var milisecondFrames=duration%(frameTime*maxFrame);
 		var currentFrame=parseInt(milisecondFrames/(frameTime));
-		context.drawImage(prv.image //grafika
-			,44*currentFrame , pub.getHeight()*(prv.direction>0?5:6) //punkt od którego kadrować
-			,44,pub.getHeight() //skala obrazka
-			,prv.pos.x,prv.pos.y+1 //współrzędne rysowania
-			,44,pub.getHeight()//wielkość obrazka
+		context.drawImage(prv.image //image
+			,44*currentFrame , pub.getHeight()*(prv.direction>0?5:6) //cut point
+			,44,pub.getHeight() //image scale
+			,prv.pos.x,prv.pos.y+1 //image coordiate
+			,44,pub.getHeight()//image size
 		);
 
 	}
 
+	/**
+	 * Character fall animation
+	 *
+	 * @param CanvasRenderingContext2D context
+	 * @param long drawStart epoch
+	 * @param long diff diffrent time between frames
+	 */
 	prv.updateFall=function(context,drawStart,diff){
 		var frameTime=150;
 		var maxFrame=5;
 		var duration=drawStart-prv.stateTime;
 		var milisecondFrames=duration%(frameTime*maxFrame);
 		var currentFrame=parseInt(milisecondFrames/(frameTime))+4;
-		context.drawImage(prv.image //grafika
-			,44*currentFrame , pub.getHeight()*(prv.direction>0?5:6) //punkt od którego kadrować
-			,44,pub.getHeight() //skala obrazka
-			,prv.pos.x,prv.pos.y+1 //współrzędne rysowania
-			,44,pub.getHeight()//wielkość obrazka
+		context.drawImage(prv.image //image
+			,44*currentFrame , pub.getHeight()*(prv.direction>0?5:6) //cut point
+			,44,pub.getHeight() //image scale
+			,prv.pos.x,prv.pos.y+1 //image coordiate
+			,44,pub.getHeight()//image size
 		);
 
 	}
 
+	/**
+	 * Character restore animation
+	 *
+	 * @param CanvasRenderingContext2D context
+	 * @param long drawStart epoch
+	 * @param long diff diffrent time between frames
+	 */
 	prv.updateRestore=function(context,drawStart,diff){
 		var frameTime=150;
 		var maxFrame=8;
 		var duration=drawStart-prv.stateTime;
 		var milisecondFrames=duration%(frameTime*maxFrame);
 		var currentFrame=parseInt(milisecondFrames/(frameTime));
-		context.drawImage(prv.image //grafika
-			,44*currentFrame , pub.getHeight()*0 //punkt od którego kadrować
-			,44,pub.getHeight() //skala obrazka
-			,prv.pos.x,prv.pos.y+1 //współrzędne rysowania
-			,44,pub.getHeight()//wielkość obrazka
+		context.drawImage(prv.image //image
+			,44*currentFrame , pub.getHeight()*0 //cut point
+			,44,pub.getHeight() //image scale
+			,prv.pos.x,prv.pos.y+1 //image coordiate
+			,44,pub.getHeight()//image size
 		);
 
 	}
 
+	/**
+	 * Character kick animation
+	 *
+	 * @param CanvasRenderingContext2D context
+	 * @param long drawStart epoch
+	 * @param long diff diffrent time between frames
+	 */
 	prv.updateKick=function(context,drawStart,diff){
 		var frameTime=150;
 		var maxFrame=4;
 		var duration=drawStart-prv.stateTime;
 		var milisecondFrames=duration%(frameTime*maxFrame);
 		var currentFrame=parseInt(milisecondFrames/(frameTime));
-		context.drawImage(prv.image //grafika
-			,44*currentFrame , pub.getHeight()*(prv.direction>0?7:8) //punkt od którego kadrować
-			,44,pub.getHeight() //skala obrazka
-			,prv.pos.x,prv.pos.y+1 //współrzędne rysowania
-			,44,pub.getHeight()//wielkość obrazka
+		context.drawImage(prv.image //image
+			,44*currentFrame , pub.getHeight()*(prv.direction>0?7:8) //cut point
+			,44,pub.getHeight() //image scale
+			,prv.pos.x,prv.pos.y+1 //image coordiate
+			,44,pub.getHeight()//image size
 		);
 
 	}
 
+	/**
+	 * Get height character
+	 *
+	 * return int
+	 */
 	pub.getHeight=function(){
 		return 60;
 	}
@@ -239,11 +323,29 @@ function Character(x,y,engine){
 
 }
 
+/**
+ * Ground class
+ *
+ * @author Michal Tomczak (m.tomczak@coderdojo.org.pl)
+ */
 function Ground(width,height){
+	/**
+	 * @var private attr
+	 */
 	var prv={};
+
+	/**
+	 * @var public attr
+	 */
 	var pub=this;
 
-
+	/**
+	 * Constructor
+	 *
+	 * @param int x
+	 * @param int y
+	 * @param Game engine
+	 */
 	prv.init=function(width,height){
 		prv.image=new Image();
 		prv.image.src='ground.png';
@@ -252,22 +354,35 @@ function Ground(width,height){
 
 	}
 
+	/**
+	 * Redraw ground
+	 *
+	 * @param CanvasRenderingContext2D context
+	 * @param long drawStart epoch
+	 * @param long diff diffrent time between frames
+	 */
 	pub.update=function(context,drawStart,diff){
 		var y=prv.height-prv.image.height;
 
 		var stage=Math.ceil(prv.width/prv.image.width);
 		for(var i=0; i<stage; i++){
-			context.drawImage(prv.image //grafika
-				,0,0 //punkt od którego kadrować
+			context.drawImage(prv.image //image
+				,0,0 //cut point
 				,prv.image.width,prv.image.height //skala obrazka
-				,i*prv.image.width,y //współrzędne rysowania
-				,prv.image.width,prv.image.height //wielkość obrazka
+				,i*prv.image.width,y //image coordiate
+				,prv.image.width,prv.image.height //image size
 			);
 
 		}
 
 	}
 
+	/**
+	 * Check colision with character
+	 *
+	 * @param object pos ground position: x, y
+	 * @param int vector -1 fall, 0 stay, 1 jump
+	 */
 	pub.isColision=function(pos,vector){
 		return (pos.y>prv.height-prv.image.height && pos.y<prv.height) && (pos.x>0 && pos.x<prv.width) && (vector==-1) 
 ||  (pos.x>0 && pos.x<prv.width) && (pos.y<prv.height+prv.image.height && pos.y>prv.height-prv.image.height) && (vector==1);
@@ -277,10 +392,25 @@ function Ground(width,height){
 
 }
 
+/**
+ * Input manager class (support keyboard).
+ *
+ * @author Michal Tomczak (m.tomczak@coderdojo.org.pl)
+ */
 function InputManager(){
+	/**
+	 * @var private attr
+	 */
 	var prv={};
+
+	/**
+	 * @var public attr
+	 */
 	var pub=this;
 
+	/**
+	 * Constructor
+	 */
 	prv.init=function(){ 
 		pub.KEY_ARROW_LEFT=37;
 		pub.KEY_ARROW_UP=38;
@@ -302,6 +432,12 @@ function InputManager(){
 		});
 	}
 
+	/**
+	 * Check press key
+	 *
+	 * @param int key key code
+	 * @return boolean
+	 */
 	pub.isPress=function(key){
 		return prv.keyDown.indexOf(key)!=-1;
 	}
@@ -311,10 +447,25 @@ function InputManager(){
 
 }
 
+/**
+ * Game class
+ *
+ * @author Michal Tomczak (m.tomczak@coderdojo.org.pl)
+ */
 function Game(){
+	/**
+	 * @var private attr
+	 */
 	var prv={};
+
+	/**
+	 * @var public attr
+	 */
 	var pub=this;
 
+	/**
+	 * Constructor
+	 */
 	prv.init=function(){
 		prv.inputManager=new InputManager();
 		prv.canvas=document.querySelector('canvas');
@@ -334,10 +485,17 @@ function Game(){
  
 	}
 
+	/**
+	 * 
+	 * @return InputManager
+	 */
 	pub.getInputManager=function(){
 		return prv.inputManager;
 	}
 
+	/**
+	 * Redraw frame
+	 */
 	prv.draw=function(){
 		prv.context.clearRect(0,0,prv.canvas.width,prv.canvas.height);
 		var drawStart = Date.now();
@@ -348,15 +506,21 @@ function Game(){
 //		prv.context.restore();
 	}
 
-
+	/**
+	 * Invoke controller and redraw screen.
+	 *
+	 * @param CanvasRenderingContext2D context
+	 * @param long drawStart epoch
+	 * @param long diff diffrent time between frames
+	 */
 	prv.update=function(context,drawStart,diff){
 		prv.character.controller(drawStart,diff);
 
-		context.drawImage(prv.background //grafika
-			,0,0 //punkt od którego kadrować
-			,prv.background.width,prv.background.height-60 //skala obrazka
-			,0,0 //współrzędne rysowania
-			,prv.canvas.width,prv.canvas.height //wielkość obrazka
+		context.drawImage(prv.background //image
+			,0,0 //cut point
+			,prv.background.width,prv.background.height-60 //image scale
+			,0,0 //image coordiate
+			,prv.canvas.width,prv.canvas.height //image size
 		);
 
 		prv.ground.update(context,drawStart,diff);
@@ -364,10 +528,18 @@ function Game(){
 		prv.character.update(context,drawStart,diff);
 	}
 
+	/**
+	 *
+	 * @return Ground
+	 */
 	pub.getGround=function(){
 		return prv.ground;
 	}
 
+	/**
+	 *
+	 * @return Ground
+	 */
 	pub.getGround2=function(){
 		return prv.ground2;
 	}
